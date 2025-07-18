2a677846e5207d081127d24e1e520b9d50fa5ebf
nikolaukandrej <nikolaukandrej@gmail.com>
2025-07-18 20:44:23 +0300

    Hibernate

---
+++ src/main/java/by/nikolauk/spring/HibernateApp/App.java
@@ -0,0 +1,34 @@
+package by.nikolauk.spring.HibernateApp;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import by.nikolauk.spring.model.Person;
+
+public class App {
+	public static void main(String[] args) {
+		Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+
+		SessionFactory sessionFactory = configuration.buildSessionFactory();
+		Session session = sessionFactory.getCurrentSession();
+
+		try {
+			session.beginTransaction();
+
+			Person person = session.get(Person.class, 1);
+
+			if (person != null) {
+				System.out.println(person.getName());
+				System.out.println(person.getAge());
+			} else {
+				System.out.println("Человек с id=1 не найден.");
+			}
+
+			session.getTransaction().commit();
+
+		} finally {
+			sessionFactory.close();
+		}
+	}
+}
